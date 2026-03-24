63697cd3da9f3eb18bf24a405eae0ac716b6ba31
= <=>
2026-03-24 17:57:21 -0300

    feat: add method calcularSalario

--- src/com/fornazeiro/Empregado.java
+++ src/com/fornazeiro/Empregado.java
@@ -4,6 +4,57 @@ public class Empregado {
 
     private String nome;
     private int idade;
+    private int tipo;
+    private double salario;
+    private double comissao;
+    private double bonus;
+
+    public double calcularSalario(){
+        if (tipo == 1){
+            return salario;
+        }
+        else if (tipo == 2){
+            return salario + salario * comissao;
+        }
+        else if (tipo == 3){
+            return salario + bonus;
+        }
+        else{
+            return 0.;
+        }
+    }
+
+    public int getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(int tipo) {
+        this.tipo = tipo;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public double getComissao() {
+        return comissao;
+    }
+
+    public void setComissao(double comissao) {
+        this.comissao = comissao;
+    }
+
+    public double getBonus() {
+        return bonus;
+    }
+
+    public void setBonus(double bonus) {
+        this.bonus = bonus;
+    }
 
     public String getNome() {
         return nome;
