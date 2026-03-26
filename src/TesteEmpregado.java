0b1636944a0c525d58a8a2df558ddb4a656aa184
= <=>
2026-03-26 15:30:12 -0300

    feat: add new learn of variables in java

--- src/TesteEmpregado.java
+++ src/TesteEmpregado.java
@@ -2,11 +2,16 @@ import com.fornazeiro.Empregado;
 
 public class TesteEmpregado {
     public static void main(String[] args) {
+
+        String nome = "Felipe";
+        String apelido = nome; //sempre sera felipe
+        nome = "mario";
+        System.out.println(apelido);
+
         Empregado emp1 = new Empregado();
         emp1.setNome("Felipe");
         emp1.setTipo(2);
         emp1.setSalario(2000);
         emp1.setComissao(0.1);
-        System.out.println(emp1.calcularSalario());
     }
 }
