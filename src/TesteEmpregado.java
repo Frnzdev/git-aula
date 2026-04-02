3986ee34c9d84be7a3513a169fec2620ffa54bfe
frnzdev <felipeacfornazeiro@gmail.com>
2026-04-02 15:36:24 -0300

    updating

--- src/TesteEmpregado.java
+++ src/TesteEmpregado.java
@@ -1,7 +1,8 @@
 import com.fornazeiro.Empregado;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class TesteEmpregado {
     public static void main(String[] args) {
@@ -12,28 +13,45 @@ public class TesteEmpregado {
         name = "Mario";
         System.out.println(apelido); felipe
          */
-
+        //ta com esse erro de write only object, pois eu não usei em lugar algum, apenas instaciei o objeto
         Empregado e1 = new Empregado();
         Empregado e2 = new Empregado();
         Empregado e3 = new Empregado();
+        Empregado e4 = new Empregado();
 
         e1.setNome("Marcos");
         e2.setNome("Joao");
         e3.setNome("Pedro");
 
-
         e1.setTipo(1);
         e2.setTipo(2);
         e3.setTipo(3);
+        e4.setTipo(3);
+
+
 
         e1.setSalario(2000);
         e2.setSalario(1700);
         e2.setComissao(0.2);//20% de comissão sobre o salário
         e3.setSalario(1500);
         e3.setBonus(400);
+        e4.setSalario(1500);
+        e4.setBonus(400);
 
-        List<Empregado> emps = Arrays.asList(e1, e2, e3);
+        Scanner s = new Scanner(System.in);
+        List<Empregado> emps = new ArrayList<>();
 
+
+        for (int i = 0; i >= emps.size() && emps.size() <= 3 ; i++) {
+            Empregado newEmp = new Empregado();
+            System.out.println("digite seu nome");
+            String nome = s.next();
+            newEmp.setNome(nome);
+            System.out.println("digite sua idade");
+            int idade = s.nextInt();
+            newEmp.setIdade(idade);
+            emps.add(newEmp);
+        }
         for (Empregado e : emps) {
             System.out.println("---------------------");
             System.out.println("Nome: " + e.getNome());
